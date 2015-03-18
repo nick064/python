096a8700c49c1cce3b6c0fb7deeaa9afd3691013
Kolio <anastasov@nbrain.net>
2015-03-18 09:24:32 +0200

    Delete test.py

--- test.py
+++
@@ -1,5 +0,0 @@
-def getTwoNumbers():
-    numberA = raw_input("Enter your first number: ")
-    numberB = raw_input("Enter your second number: ")
-    return int(numberA), int(numberB)
-    print "The numbers were: %d %d" % (numberA, numberB)
